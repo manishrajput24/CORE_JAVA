6b22ad1a9b84c169799e04f67866c8831a18635c
Manish Rajput <95074168+manishrajput24@users.noreply.github.com>
2023-04-08 13:54:34 +0530

    Delete Ex1.java

--- Ex1.java
+++
@@ -1,7 +0,0 @@
-// --------Write a program that displays Hello World-------//
-
-public class Ex1 {
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
-}
