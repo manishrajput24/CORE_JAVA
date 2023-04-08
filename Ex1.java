daadd6c1340bc1fe315229633490896f0b00439c
manishrajput24 <manishrajput2442@gmail.com>
2023-04-08 13:48:42 +0530

    msg

---
+++ Ex1.java
@@ -0,0 +1,7 @@
+// --------Write a program that displays Hello World-------//
+
+public class Ex1 {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
---
+++ Ex2.java
@@ -0,0 +1,12 @@
+// ---------Write a program that displays Hello World 5 times------//
+
+public class Ex2 {
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count < 5) {
+            System.out.print("Hello World\n");
+            count++;
+        }
+    }
+}
